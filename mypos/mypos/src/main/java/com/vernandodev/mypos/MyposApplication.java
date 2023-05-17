30c84c7bcca2d5e8048e8a9171a01c540f081f9b
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-05-17 09:01:12 +0700

    update

---
+++ mypos/mypos/src/main/java/com/vernandodev/mypos/MyposApplication.java
@@ -0,0 +1,13 @@
+package com.vernandodev.mypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyposApplication.class, args);
+	}
+
+}
---
+++ mypos/mypos/src/test/java/com/vernandodev/mypos/MyposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vernandodev.mypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
